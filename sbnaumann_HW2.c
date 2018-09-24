b5618535ff6ddd6aed151f6874c2125e97dcada6
sbnaumann <33404072+sbnaumann@users.noreply.github.com>
2018-09-24 10:23:25 -0400

    Add files via upload

---
+++ sbnaumann_HW2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+
+
+
+//This is the "little bit of math section"
+/*
+Problem 1: 
+    A) 1111111111 = 2^10 + 2^9 + 2^8 + 2^7 + 2^6 + 2^5 + 2^4 + 2^3 + 2^2 + 2^1 +2^0 = 1 + 2 + 4 + 8 + 16 + 32 + 64 + 128 + 256 + 
+    B) 
+ */ 
